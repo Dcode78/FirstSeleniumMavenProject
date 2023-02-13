dbbf159e8a584ec7ca5b77c13f4a5711e4e7f4cf
Dcode78 <d.s.uduwage@gmail.com>
2023-02-13 15:00:17 +0000

    1st commit

--- src/test/java/LTPlayGroundTest.java
+++ src/test/java/LTPlayGroundTest.java
@@ -1,9 +1,11 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,15 +14,12 @@ public class LTPlayGroundTest {
     public void validateCheckBoxStatus (){
         /**WebDriverManager is a open Source API to ease the Browser Initialisation .
         We can skip driver exe path definition**/
-       WebDriverManager.firefoxdriver().setup();
-
-        //Chrome Browser Initialised
-        WebDriver driver= new FirefoxDriver();
+        WebDriver driver = WebDriverManager.firefoxdriver().create();
 
         //Launch the URL
         driver.get("https://www.lambdatest.com/selenium-playground/checkbox-demo");
+        
         //get the WebElement of Check Box
-
         WebElement singleCheckBox= driver.findElement(By.id("isAgeSelected"));
 
     System.out.println("Type of WebElement "+String.valueOf(singleCheckBox instanceof WebElement) );
